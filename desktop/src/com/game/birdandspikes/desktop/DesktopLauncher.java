6f19d38d1f0d27f8e123662656025afce28ebca5
Unknown <fafusekp@gmail.com>
2018-03-03 17:20:57 +0100

    Add basical project files

---
+++ desktop/src/com/game/birdandspikes/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.game.birdandspikes.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.game.birdandspikes.BirdAndSpikes;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BirdAndSpikes(), config);
+	}
+}
