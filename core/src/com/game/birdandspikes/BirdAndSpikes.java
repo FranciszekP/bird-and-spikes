ddfe461cd6988aee4fed82dc568f301ac287bb8f
Unknown <fafusekp@gmail.com>
2018-03-03 18:15:45 +0100

    Add state files AGAIN

--- core/src/com/game/birdandspikes/BirdAndSpikes.java
+++ core/src/com/game/birdandspikes/BirdAndSpikes.java
@@ -5,29 +5,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.game.birdandspikes.states.GameStateManager;
+import com.game.birdandspikes.states.MenuState;
 
 public class BirdAndSpikes extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private GameStateManager gsm;
 
 	@Override
-	public void create() {
+	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
 	}
 
 	@Override
-	public void render() {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
 
 	@Override
-	public void dispose() {
+	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
