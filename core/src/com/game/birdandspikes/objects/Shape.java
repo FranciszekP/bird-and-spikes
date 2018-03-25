2da6f2424b47d1bc3df1d8911e5a5ab8bfbd1d81
Przemysław Widera <przemyslawwidera@Mac-mini-Przemysaw.local>
2018-03-25 18:36:25 +0200

    Add circle conversion
    taka moja mała fanaberia

--- core/src/com/game/birdandspikes/objects/Shape.java
+++ core/src/com/game/birdandspikes/objects/Shape.java
@@ -2,6 +2,7 @@ package com.game.birdandspikes.objects;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Polygon;
 
 /**
@@ -9,6 +10,10 @@ import com.badlogic.gdx.math.Polygon;
  */
 
 public class Shape {
+    protected final static float SIN_22_5 = 0.383f;
+    protected final static float COS_22_5 = 0.924f;
+    protected final static float SQRT_2 = 1.414f;
+
     protected float[] vertices;
     protected Polygon shape;
     protected Texture texture;
@@ -39,5 +44,28 @@ public class Shape {
 
     public void setVertices(float[] vertices) {
         this.vertices = vertices;
+    }
+
+    public float[] turnIntoVertices(Circle circle) { // https://drive.google.com/open?id=1EgcGb-gkcBGolVkuFjgNb2D-mVsArPTn
+        float radius = circle.radius;
+        float x = radius * COS_22_5;
+        float h = radius * SIN_22_5;
+        float a = (radius * SQRT_2) / 2;
+        return new float[] { circle.x - radius, circle.y, // 1
+                circle.x - x, circle.y + h, // 2
+                circle.x - a, circle.y + a, // 3
+                circle.x - h, circle.y + x, // 4
+                circle.x, circle.y + radius, // 5
+                circle.x + h, circle.y + x, // 6
+                circle.x + a, circle.y + a, // 7
+                circle.x + x, circle.y + h, // 8
+                circle.x + radius, circle.y, // 9
+                circle.x + x, circle.y - h, // 10
+                circle.x + a, circle.y - a, // 11
+                circle.x + h, circle.y - x, // 12
+                circle.x, circle.y - radius, // 13
+                circle.x - h, circle.y - x, // 14
+                circle.x - a, circle.y - a, // 15
+                circle.x - x, circle.y - h };// 16
     }
 }
